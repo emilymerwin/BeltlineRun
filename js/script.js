b33b1854f45614a0bda6e9f6a7b225ae35168e90
Emily Merwin <emilymerwinajc@gmail.com>
2014-04-02 19:57:49 -0400

    Move js and css into own files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function() {
+	var map = L.map('map').setView([33.768682989507914, -84.36510918661952], 13);
+	L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>, Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+		maxZoom: 18
+	}).addTo(map);
+	
+	//add GeoJSON to map as a layer
+	var featureLayer = L.mapbox.featureLayer()
+	    .loadURL('data/map.geojson')
+	    .addTo(map);
+}());
