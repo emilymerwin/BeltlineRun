00e2c6fe615048890d4ef5911825f80ecca1655f
Emily Merwin <emilymerwinajc@gmail.com>
2014-04-02 16:03:01 -0400

    Remove placeholder text

--- py/createJSON.py
+++ py/createJSON.py
@@ -40,7 +40,7 @@ for f in files:
 	lon = -1*float(decimal.Decimal(GPS[4][0][0]/GPS[4][0][1]) + decimal.Decimal(GPS[4][1][0]/GPS[4][1][1])/60 + decimal.Decimal(GPS[4][2][0]/GPS[4][2][1])/3600) #convert to decimal format, make negative for Western lons (all of ours)
 
 	#store the image properties we want for the JSON
-	slides.append({"media": {"url": f.strip("../"), "credit": "Ben Gray/AJC", "caption": get_field(exif, 'ImageDescription')}, "date": get_field(exif, "DateTimeOriginal"), "location": {"lat": lat, "lon": lon, "zoom": 15}, "text": {"headline": "Headline (optional)", "text": "This is where you would put a caption. Timestamp: "+get_field(exif, 'DateTimeOriginal')}})
+	slides.append({"media": {"url": f.strip("../"), "credit": "Ben Gray/AJC", "caption": ""}, "date": get_field(exif, "DateTimeOriginal"), "location": {"lat": lat, "lon": lon, "zoom": 15}, "text": {"headline": "", "text": get_field(exif, 'DateTimeOriginal')}})
 
 #put the images in chronological order
 slides.sort(key=operator.itemgetter("date"))
