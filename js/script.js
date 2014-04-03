871ad4d2d547e90782e8c2502bea39abbd765dfa
Emily Merwin <emilymerwinajc@gmail.com>
2014-04-03 05:44:10 -0400

    Display tweaks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //(function() { //commented so I can access vars in development
 	var map = L.map('map').setView([33.768682989507914, -84.36510918661952], 13);
-	L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+	L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
 		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>, Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
 		maxZoom: 18
 	}).addTo(map);
