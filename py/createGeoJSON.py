bec4359b13d22c4ac99f79c82d0d8bb38d0a677e
Emily Merwin <emilymerwinajc@gmail.com>
2014-04-09 17:33:43 -0400

    Better comments for .py parsers

--- py/createGeoJSON.py
+++ py/createGeoJSON.py
@@ -1,40 +1,44 @@
 #! /usr/bin/env python
-#this script will open all of the files in a directory, assume they are images, and extract metadata for storymap.js
+#this script will open all of the files in a directory, assume they are images, extract metadata and generate a .geoJSON file
 
-import os.path
+import os.path #for traversing system directories
 import json
-import decimal
-#https://github.com/python-imaging/Pillow
-from PIL import Image
+import decimal #for GPS conversion
+from PIL import Image #for reading image EXIF data, using this fork https://github.com/python-imaging/Pillow
 from PIL.ExifTags import TAGS
 import operator
 
-directory = '../img/resized/' #where to find the images
+#where to find the images
+directory = '../img/resized/'
 
-#create slides array and add Ben's run line
+#set up the geoJSON object
 geoJ = {
 	"type": "FeatureCollection",
 	"features": []
 }
 
-def get_field (exif,field) :
+#return value of requested key
+def get_field (exif,field):
 	for (k,v) in exif.iteritems():
 		if TAGS.get(k) == field:
 			return v
 
-files = [os.path.join(directory, f) for f in os.listdir(directory) if not f.startswith('.') #search directory for files, exclude hidden files
-	if os.path.isfile(os.path.join(directory, f))] #Get only files, not directories, join the string
+#search directory for files, exclude hidden files
+files = [os.path.join(directory, f) for f in os.listdir(directory) if not f.startswith('.')
+	#Get only files, not directories, join the string
+	if os.path.isfile(os.path.join(directory, f))]
 
-for f in files:
-	#parse keys
-	# for (key ,val) in Image.open(f)._getexif().iteritems():
+	#print keys, so you can see what's available and value formats
+	# for (key ,val) in Image.open(files[0])._getexif().iteritems():
 	# 	print '%s = %s' % (TAGS.get(key), val)
 
+for f in files:
 	exif = Image.open(f)._getexif()
 	GPS = get_field(exif, 'GPSInfo')
-	lat = float(decimal.Decimal(GPS[2][0][0]/GPS[2][0][1]) + decimal.Decimal(GPS[2][1][0]/GPS[2][1][1])/60 + decimal.Decimal(GPS[2][2][0]/GPS[2][2][1])/3600) #convert to decimal format
-	lon = -1*float(decimal.Decimal(GPS[4][0][0]/GPS[4][0][1]) + decimal.Decimal(GPS[4][1][0]/GPS[4][1][1])/60 + decimal.Decimal(GPS[4][2][0]/GPS[4][2][1])/3600) #convert to decimal format, make negative for Western lons (all of ours)
-
+	#convert to decimal format
+	lat = float(decimal.Decimal(GPS[2][0][0]/GPS[2][0][1]) + decimal.Decimal(GPS[2][1][0]/GPS[2][1][1])/60 + decimal.Decimal(GPS[2][2][0]/GPS[2][2][1])/3600)
+	#convert to decimal format, make negative for Western lons (all of ours)
+	lon = -1*float(decimal.Decimal(GPS[4][0][0]/GPS[4][0][1]) + decimal.Decimal(GPS[4][1][0]/GPS[4][1][1])/60 + decimal.Decimal(GPS[4][2][0]/GPS[4][2][1])/3600)
 	#store the image properties we want for the JSON
 	geoJ["features"].append({"type": "Feature", "geometry": {"type": "Point", "coordinates": [lon, lat]}, "properties": {"marker-symbol": "camera", "marker-size": "small", "marker-color": "#367498", "date": get_field(exif, "DateTimeOriginal"), "caption": get_field(exif, "ImageDescription"), "width": str(get_field(exif, "ExifImageWidth"))+"px", "height": str(get_field(exif, "ExifImageHeight"))+"px", "image": f.strip("../") }})
 
--- py/jillGeoJSON.py
+++ py/jillGeoJSON.py
@@ -1,46 +1,47 @@
 #! /usr/bin/env python
-#this script will open all of the files in a directory, assume they are images, and extract metadata for storymap.js
+#this script will open all of the files in a directory, assume they are images, extract metadata and generate a .geoJSON file
 
-import os.path
+import os.path #for traversing system directories
 import json
-import decimal
-#https://github.com/python-imaging/Pillow
-from PIL import Image
+import decimal #for GPS conversion
+from PIL import Image #for reading image EXIF data, using this fork https://github.com/python-imaging/Pillow
 from PIL.ExifTags import TAGS
 import operator
 
-directory = '../img/jill/starred/' #where to find the images
+#where to find the images
+directory = '../img/jill/starred/'
 
-#create slides array and add Ben's run line
+#set up the geoJSON object
 geoJ = {
 	"type": "FeatureCollection",
 	"features": []
 }
 
-def get_field (exif,field) :
+#return value of requested key
+def get_field (exif,field):
 	for (k,v) in exif.iteritems():
 		if TAGS.get(k) == field:
 			return v
-	
-files = [os.path.join(directory, f) for f in os.listdir(directory) if not f.startswith('.') #search directory for files, exclude hidden files
-	if os.path.isfile(os.path.join(directory, f))] #Get only files, not directories, join the string
 
-for f in files:
-	#parse keys
-	# for (key ,val) in Image.open(f)._getexif().iteritems():
+#search directory for files, exclude hidden files
+files = [os.path.join(directory, f) for f in os.listdir(directory) if not f.startswith('.')
+	#Get only files, not directories, join the string
+	if os.path.isfile(os.path.join(directory, f))]
+
+	#print keys, so you can see what's available and value formats
+	# for (key ,val) in Image.open(files[0])._getexif().iteritems():
 	# 	print '%s = %s' % (TAGS.get(key), val)
 
+for f in files:
 	exif = Image.open(f)._getexif()
 	GPS = get_field(exif, 'GPSInfo')
-	lat = float(decimal.Decimal(GPS[2][0][0]/GPS[2][0][1]) + decimal.Decimal(GPS[2][1][0]/GPS[2][1][1])/60 + decimal.Decimal(GPS[2][2][0]/GPS[2][2][1])/3600) #convert to decimal format
-	lon = -1*float(decimal.Decimal(GPS[4][0][0]/GPS[4][0][1]) + decimal.Decimal(GPS[4][1][0]/GPS[4][1][1])/60 + decimal.Decimal(GPS[4][2][0]/GPS[4][2][1])/3600) #convert to decimal format, make negative for Western lons (all of ours)
+	#convert to decimal format
+	lat = float(decimal.Decimal(GPS[2][0][0]/GPS[2][0][1]) + decimal.Decimal(GPS[2][1][0]/GPS[2][1][1])/60 + decimal.Decimal(GPS[2][2][0]/GPS[2][2][1])/3600)
+	#convert to decimal format, make negative for Western lons (all of ours)
+	lon = -1*float(decimal.Decimal(GPS[4][0][0]/GPS[4][0][1]) + decimal.Decimal(GPS[4][1][0]/GPS[4][1][1])/60 + decimal.Decimal(GPS[4][2][0]/GPS[4][2][1])/3600)
 	#store the image properties we want for the JSON
 	geoJ["features"].append({"type": "Feature", "geometry": {"type": "Point", "coordinates": [lon, lat]}, "properties": {"marker-symbol": "star", "marker-size": "small", "marker-color": "#FC7234", "text": get_field(exif, "ImageDescription")} })
 
-#dates.sort()
-# for i, feature in geoJ["features"]:
-# 	print i
-#geoJ["features"].sort(key=operator.itemgetter("date"))
 #turn tree object into JSON and pretty print it
 out = json.dumps(geoJ, indent=4, separators=(',', ': '))
 print "JSON parsed!"
